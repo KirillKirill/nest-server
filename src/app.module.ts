fde91b409e1a977514a0b1a8757a073e6a673542
kirill <axe777@inbox.ru>
2020-07-17 15:21:08 +0300

    fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
-import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
-    CatsModule,
     UsersModule,
   ],
   controllers: [AppController],
