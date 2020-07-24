27652c85a1efe26026f1955b1d1c1017089ebc37
kirill <axe777@inbox.ru>
2020-07-24 15:51:07 +0300

    add validation for every field when user registering

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
 import { UsersModule } from './users/users.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { BadRequestExceptionFilter } from './exceptionFilter/badRequestExceptionFilter';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     UsersModule,
     AuthenticationModule,
+    BadRequestExceptionFilter,
   ],
   controllers: [],
   providers: [],
